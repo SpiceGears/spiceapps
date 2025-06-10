d1b6195e149f5e7446189cb272eb8ea503eaa059
Michal Kulik <kulikkmichal@icloud.com>
2025-06-10 20:09:27 +0200

    docker hot reload'

--- spicehub/next.config.ts
+++ spicehub/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+interface WatchOptions {
+  poll: number;
+  aggregateTimeout: number;
+}
+
+interface WebpackDevMiddlewareConfig {
+  watchOptions?: WatchOptions;
+  [key: string]: any;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config: WebpackDevMiddlewareConfig): WebpackDevMiddlewareConfig => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
