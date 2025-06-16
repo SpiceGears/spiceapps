3ea62e03ad4edc92615d0ab52d34a6212eaa72fc
Michal Kulik <kulikkmichal@icloud.com>
2025-06-16 22:12:53 +0200

    broken login 2
    that one person that mentioned me in the recruitment process please help me :sob:

--- spicehub-mobile/app/(auth)/login.tsx
+++ spicehub-mobile/app/(auth)/login.tsx
@@ -37,7 +37,7 @@ export default function LoginScreen() {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(payload)
+                body: JSON.stringify()
             })
         } catch (e) {
 
