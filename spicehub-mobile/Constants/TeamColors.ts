5cb678c21af82860020a7b1e0f63cdfdfa30586e
Michal Kulik <kulikkmichal@icloud.com>
2025-06-19 17:43:35 +0200

    spicehub mobile task page header

---
+++ spicehub-mobile/Constants/TeamColors.ts
@@ -0,0 +1,8 @@
+export enum COLORS  {
+    MECHANICS = "#38b6ff",
+    PROGRAMMERS = "#7ed957",
+    MTR = "#df0089",
+    EXECUTIVE = "#f4ca24",
+    MARKETING = "#4718a4",
+    NEUTRAL = "#a8a8a8"
+}
--- spicehub-mobile/app/project/[id].tsx
+++ spicehub-mobile/app/project/[id].tsx
@@ -1,11 +1,81 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
-
+import { router, useLocalSearchParams } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Alert, Pressable, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/Constants/TeamColors';
+import { useEffect, useState } from 'react';
+import { Project } from '@/models/Project';
+import { BackendUrl } from '@/Constants/backend';
+import * as SecureStore from 'expo-secure-store';
 
 export default function Task() {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const [project, setProject] = useState<Project | null>(null);
+  const [isProjectMenuOpen, setIsProjectMenuOpen] = useState(true || false); 
+
+  useEffect(() => {
+    async function fetchProject() {
+      try {
+        const token = await SecureStore.getItemAsync('accessToken');
+        if (!token) return;
+
+        const res = await fetch(`${BackendUrl}/api/project/${id}`, {
+          method: 'GET',
+          headers: {
+            Authorization: token,
+          },
+        });
+        if (!res.ok) throw new Error(await res.text());
+        setProject(await res.json());
+      } catch (e: any) {
+        Alert.alert('Error', e.message || e.toString());
+      }
+    }
+    fetchProject();
+  }, [id]);
+
+  function getHeaderColor(scopes?: string[]): string {
+    if (!scopes?.length || scopes?.length > 1) {
+      return COLORS.NEUTRAL;
+    }
+
+    for (let scope of scopes) {
+      const key = scope.trim().toUpperCase() as keyof typeof COLORS;
+      if (key in COLORS) {
+        return COLORS[key];
+      }
+    }
+
+    return COLORS.NEUTRAL;
+  }
+
+  const headerBg = getHeaderColor(project?.scopesRequired);
+
   return (
-    <View>
-      <Text className="text-3xl font-bold text-blue-500">Test Test TEst</Text>
-    </View>
-  )
+    <SafeAreaView
+      edges={['top']}
+      className="bg-transparent"
+      style={{ backgroundColor: headerBg }}
+    >
+      <View className="px-4 py-5">
+        <View className="flex-row justify-between items-center">
+          <View className="flex-row items-center">
+            <Pressable onPress={() => router.replace('/(tabs)/spicelab')}>
+              <Ionicons name="arrow-back" size={22} color="#000" />
+            </Pressable>
+            <Text className="text-[20px] ml-5">Project</Text>
+          </View>
+          <Pressable onPress={() => setIsProjectMenuOpen(true)}>
+            <Ionicons name="ellipsis-horizontal" size={28} color="#000" />
+          </Pressable>
+        </View>
+        <View className="flex-row items-center mt-8">
+          <Ionicons name="folder" size={25} color="#000" />
+          <Text className="text-2xl font-bold text-black ml-5">
+            {project?.name}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
