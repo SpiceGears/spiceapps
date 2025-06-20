3b69dc6c9c1ac3eaf45810f83148540081b37725
Michal Kulik <kulikkmichal@icloud.com>
2025-06-20 13:03:10 +0200

    spicehub mobile colors fix

--- spicehub-mobile/Constants/TeamColors.ts
+++ spicehub-mobile/Constants/TeamColors.ts
@@ -1,7 +1,7 @@
 export enum COLORS  {
     MECHANICS = "#38b6ff",
     PROGRAMMERS = "#7ed957",
-    MTR = "#df0089",
+    SOCIALMEDIA = "#df0089",
     EXECUTIVE = "#f4ca24",
     MARKETING = "#4718a4",
     NEUTRAL = "#a8a8a8"
