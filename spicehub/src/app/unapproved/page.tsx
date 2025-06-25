457e15705614f6e36a17a75d07b42bcc46e8f286
Bloodhatam <65913180+Bloodhatam@users.noreply.github.com>
2025-06-25 12:16:53 +0200

    fix too many redirects on unapproved user page
    redirect loop is not fun imo

--- spicehub/src/app/unapproved/page.tsx
+++ spicehub/src/app/unapproved/page.tsx
@@ -6,6 +6,9 @@ const page = () => {
         <h1 className='text-5xl'>Zostałeś zalogowany,</h1>
         <h2 className='text-4xl'>ale nadal nie zostałeś zatwierdzony.</h2>
         <h3 className='text-lg'>Skontaktuj się z administratorem w celu zatwierdzenia twojego konta.</h3>
+
+        <a href='/logout' className='mt-2 text-blue-600 dark:text-blue-500 
+                         hover:text-blue-700 dark:hover:text-blue-600'>Wyloguj się</a>
     </div>
   )
 }
